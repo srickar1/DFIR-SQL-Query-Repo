9bab11f2d4645fa02665cdb76c4aaae1a4bbff15
srickar1 <160101492+srickar1@users.noreply.github.com>
2025-11-13 04:53:16 +0000

    Update TikTokMessages.sql
    Updated SQL Query based on updated columns and research as of November 2025.

--- iOS/TIKTOK/TikTokMessages.sql
+++ iOS/TIKTOK/TikTokMessages.sql
@@ -1,27 +1,33 @@
 /* For this query to run the AwemeIM.db database must be attached to your working  
 messages table. The messages table directory has a 19 digit long filename and the database within the directory is named db.sqlite. You have to attach one of the  
 tables to the other in order for this query to run as-is. This query was tested in DB Browser for SQLite  
-verion 3.10.1. For details see blog post here: https://abrignoni.blogspot.com/2018/11/finding-tiktok-messages-in-ios.html     
-By: Alexis Brignoni  
-Twitter: @AlexisBrignoni  
-Blog: abrignoni.blogspot.com  
+verion 3.12.2. 
+For details see: https://dfir.pubpub.org/pub/h6vyh33u/release/1 and/or https://systoolsgroup.com/updates/retrieve-messages-from-tiktok/
+By: S. Rickard     
+Forked from: Alexis Brignoni
+For details see blog post here: https://abrignoni.blogspot.com/2018/11/finding-tiktok-messages-in-ios.html
+	Twitter: @AlexisBrignoni  
+	Blog: abrignoni.blogspot.com  
 */
 
-select
-sender,
-url1 as profilepicURL,
-customid,
-nickname,
-datetime(localcreatedat, 'unixepoch', 'localtime') as Local_Create_Time,
-	case 
-		when servercreatedat > 1 then datetime(servercreatedat, 'unixepoch', 'localtime')
-		else servercreatedat
-	end
-servercreatedat,
-json_extract(content, '$.text') as message,
-json_extract(content, '$.tips') as localresponse,
-json_extract(content,'$.display_name') as links_display_name,
-json_extract(content, '$.url.url_list[0]') as links_gifs_urls
-from TIMMessageORM, awemecontacts
-where uid = sender
-order by Local_Create_Time
+SELECT
+TIMMessageORM.belongingConversationIdentifier AS "Conversation Identifier",
+AwemeIM.AwemeContactsV6.nickname AS "Sender",
+TIMMessageORM.sender AS "Sender ID",
+datetime (TIMMessageORM.localCreatedAt, 'UNIXEPOCH', 'lOCALTIME') AS "Timestamp (Local)",
+CASE 
+	WHEN TIMMessageORM.serverCreatedAt > 0 
+	THEN datetime (TIMMessageORM.serverCreatedAt, 'UNIXEPOCH', 'LOCALTIME')
+	Else 'Message Not Sent from Device'
+END AS "Server Timestamp",
+TIMMessageORM.content AS "Message",
+TIMMessageORM.contentPb AS "Attachments",
+TIMMessageORM.scene,
+CASE 
+	WHEN TIMMessageORM.deleted = 0 then "No"
+	When TIMMessageORM.deleted = 1 then "Yes"
+End AS "Deleted Message",
+TIMMessageORM.type AS "Message Type"
+FROM TIMMessageORM
+LEFT JOIN AwemeIM.AwemeContactsV6 on AwemeContactsV6.uid=TIMMessageORM.sender
+Order by TIMMessageORM.localCreatedAt ASC
